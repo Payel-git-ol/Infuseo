d00012f74ba24bf1722644542e8593956b6a623b
Павел <pasazaharenko940@gmail.com>
2025-07-31 11:52:56 +0300

    mongouser

--- cmd/app/main.go
+++ cmd/app/main.go
@@ -1,18 +1,14 @@
 package main
 
 import (
-	"Infuseo/internal/database/sqlitedb"
+	mongodb "Infuseo/internal/database/mongo"
 	"Infuseo/internal/registretion"
 	"github.com/gofiber/fiber/v2"
 	"github.com/gofiber/template/html/v2"
-	"log"
 )
 
 func main() {
-	// Инициализация БД SQLITE
-	if err := sqlitedb.InitSqlite(); err != nil {
-		log.Fatalf("Ошибка инициализации БД SQLITE: %v", err)
-	}
+	mongodb.InitMongo()
 
 	engine := html.New("./web/templates", ".html")
 	app := fiber.New(fiber.Config{Views: engine})
@@ -22,5 +18,5 @@ func main() {
 	app.Get("/reg", registration.GetHandlerRegister)
 	app.Post("/reg", registration.PostHandlerRegister)
 
-	log.Fatal(app.Listen(":8080"))
+	app.Listen(":8080")
 }
